d768a40dc9749976cbd191b0a466c1bb28287fde
Xavier Mitault <xavier.mitault@epitech.eu>
2025-07-23 19:41:15 +0100

    Add fonts

--- install.sh
+++ install.sh
@@ -31,12 +31,21 @@ if [[ -d "$HOME/.local/share/wallpaper" ]]; then
     echo "FAILED !! ERROR !! $HOME/.local/share/wallpaper exists..."
     exit 2
 fi
+if [[ -d "$HOME/.local/share/fonts" ]]; then
+    echo "FAILED !! ERROR !! $HOME/.local/share/fonts exists..."
+    exit 2
+fi
 
 ln -sf   "$SRC/.config/i3"              "$XDG_CONFIG_HOME/i3"
 ln -sf   "$SRC/.config/rofi"            "$XDG_CONFIG_HOME/rofi"
 ln -sf   "$SRC/.config/fontconfig"      "$XDG_CONFIG_HOME/fontconfig"
 ln -sf   "$SRC/.local/share/wallpaper"  "$HOME/.local/share/wallpaper"
+ln -sf   "$SRC/.local/share/fonts"      "$HOME/.local/share/fonts"
 
 if [[ "$USER_GROUP" != "" ]]; then
     chown -R "$USER_GROUP" "$XDG_CONFIG_HOME/i3"
+    chown -R "$USER_GROUP" "$XDG_CONFIG_HOME/rofi"
+    chown -R "$USER_GROUP" "$XDG_CONFIG_HOME/fontconfig"
+    chown -R "$USER_GROUP" "$HOME/.local/share/wallpaper"
+    chown -R "$USER_GROUP" "$HOME/.local/share/fonts"
 fi
