c4d86311636fd7094a3d955bea45f3a490969dd4
Xavier Mitault <xavier.mitault@epitech.eu>
2025-07-23 19:05:01 +0100

    Add wallpaper

--- install.sh
+++ install.sh
@@ -13,13 +13,29 @@ else
 fi
 
 mkdir -p "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_DATA_HOME"
 
 if [[ -d "$XDG_CONFIG_HOME/i3" ]]; then
     echo "FAILED !! ERROR !! $XDG_CONFIG_HOME/i3 exists..."
     exit 2
 fi
+if [[ -d "$XDG_CONFIG_HOME/rofi" ]]; then
+    echo "FAILED !! ERROR !! $XDG_CONFIG_HOME/rofi exists..."
+    exit 2
+fi
+if [[ -d "$XDG_CONFIG_HOME/fontconfig" ]]; then
+    echo "FAILED !! ERROR !! $XDG_CONFIG_HOME/fontconfig exists..."
+    exit 2
+fi
+if [[ -d "$HOME/.local/share/wallpaper" ]]; then
+    echo "FAILED !! ERROR !! $HOME/.local/share/wallpaper exists..."
+    exit 2
+fi
 
-ln -sf   "$SRC/.config/i3"     "$XDG_CONFIG_HOME/i3"
+ln -sf   "$SRC/.config/i3"              "$XDG_CONFIG_HOME/i3"
+ln -sf   "$SRC/.config/rofi"            "$XDG_CONFIG_HOME/rofi"
+ln -sf   "$SRC/.config/fontconfig"      "$XDG_CONFIG_HOME/fontconfig"
+ln -sf   "$SRC/.local/share/wallpaper"  "$HOME/.local/share/wallpaper"
 
 if [[ "$USER_GROUP" != "" ]]; then
     chown -R "$USER_GROUP" "$XDG_CONFIG_HOME/i3"
